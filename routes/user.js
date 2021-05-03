2425de145ded05a05366273d857e9881ecbf62d5
hiran-hiran <aluco.hirata@gmail.com>
2021-05-04 01:27:25 +0900

    ✨ init

---
+++ routes/user.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+const { PrismaClient } = require('@prisma/client');
+
+const { user } = new PrismaClient();
+
+router.get('/', async (req, res) => {
+	const users = await user.findMany({
+		select: {
+			username: true,
+			posts: true,
+		},
+  });
+  
+  res.json(users)
+});
+module.exports = router;
